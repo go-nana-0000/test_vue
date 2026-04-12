2128df20df0a63b3e44558f95a11e378d02ba0fe
go_nana <kelso9929@gmail.com>
2026-04-13 00:42:43 +0900

    Add description

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,25 @@ import TheModel from './components/TheModel.vue'
 
 <template>
   <div id="canvas-wrapper">
-
     <TresCanvas
       :window-size="false"
       :resize="false"
       clear-color="#d2cbcb"
 
-      style="width: 800px; height: 800px;">
+      style="width: 600px; height: 600px;">
       <Suspense>
         <TheModel />
       </Suspense>
     </TresCanvas>
+  </div>
+    <div id="ui-area">
+    <h3>操作方法</h3>
+    <ul>
+      <li>Wキー：歩く</li>
+      <li> Sキー：止まる</li>
+      <li>左クリック+ドラッグ：視点回転</li>
+      <li>右クリック+ドラッグ：視点移動</li>
+      <li>マウスホイール：視点拡大/縮小</li>
+    </ul>
   </div>
 </template>
